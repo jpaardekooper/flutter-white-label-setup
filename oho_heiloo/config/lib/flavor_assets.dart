b85855195a43902f6e4522c6ee69f7e2bfa56b0e
Jasper Paardekooper <p_jazzzp@live.nl>
2021-11-07 12:14:25 +0100

    white-label test

---
+++ oho_heiloo/config/lib/flavor_assets.dart
@@ -0,0 +1,8 @@
+class FlavorAssets {
+  static const double scale = 0.5;
+  static const logo = 'config/assets/images/logo.png';
+
+  static const splash = 'config/assets/images/splash.png';
+
+  static const appBarlogo = 'config/assets/images/appbar-logo.png';
+}
---
+++ oho_heiloo/config/lib/flavor_strings.dart
@@ -0,0 +1,4 @@
+class FlavorStrings {
+  static const flavorName = 'Ondernemend Heiloo project';
+  static const homeBody = 'This is the body text for the Base project flavor.';
+}
