2abce5d6adc6bc924211b28c227f06f458a1eb2f
Jasper Paardekooper <p_jazzzp@live.nl>
2021-11-07 12:59:26 +0100

    updated flavor

--- config/lib/flavor_assets.dart
+++ config/lib/flavor_assets.dart
@@ -1,8 +1,11 @@
 class FlavorAssets {
-  static const double scale = 0.5;
+  static const double logoScale = 2;
+
+  static const double splashScale = 1;
+
+  static const double appbarLogoScale = 0.8;
+
   static const logo = 'config/assets/images/logo.png';
 
   static const splash = 'config/assets/images/splash.png';
-
-  static const appBarlogo = 'config/assets/images/appbar-logo.png';
 }
